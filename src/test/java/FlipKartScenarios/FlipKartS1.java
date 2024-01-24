f6ba8f3fbe14349c15334263e86ff8e364a8edb1
vamshikrishna428 <vamshikrishnakr@gmail.com>
2024-01-24 16:28:21 +0530

    syso

--- src/test/java/FlipKartScenarios/FlipKartS1.java
+++ src/test/java/FlipKartScenarios/FlipKartS1.java
@@ -13,14 +13,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class FlipKartS1 {
 	@Test
 	public void flipKartS1() {
-		ChromeOptions options=new ChromeOptions();
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--remote-allow-origins=*");
-		WebDriver driver=new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver(options);
 
 //		WebDriverManager.chromedriver().setup();
 //		WebDriver driver=new ChromeDriver();
@@ -29,18 +27,17 @@ public class FlipKartS1 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		ArrayList<String> list = new ArrayList<String>();
 		driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']")).click();
-		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("phones"+Keys.ENTER);
-		List<WebElement> allnames = driver.findElements(By.xpath("//div[@class='_3pLy-c row']/descendant::div[@class='_4rR01T']"));
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("phones" + Keys.ENTER);
+		List<WebElement> allnames = driver
+				.findElements(By.xpath("//div[@class='_3pLy-c row']/descendant::div[@class='_4rR01T']"));
 		for (int i = 0; i < allnames.size(); i++) {
 			String text = allnames.get(i).getText();
 			list.add(text);
 		}
 		Collections.sort(list);
-		for(String allist:list)
-		{
+		for (String allist : list) {
 			System.out.println(allist);
 		}
-		
+		driver.quit();
 	}
 }
-
